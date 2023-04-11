6e6d17b514695da5bcc85f73bbee25c0092f8046
dasdware <info@dasdware.de>
2023-04-11 19:29:57 +0200

    #3 Bugfix migration SQL, added comments

--- migrations/db-0.10.0.sql
+++ migrations/db-0.10.0.sql
@@ -1,6 +1,8 @@
 START TRANSACTION;
-
+    -- Add the has_time column.
     ALTER TABLE `trips` ADD `has_time` BOOLEAN NOT NULL DEFAULT FALSE AFTER `timestamp`;
     
-    UPDATE `trips` SET `timestamp_time` = IF(DATE_FORMAT(`timestamp`, '%H:%i:%S') = '00:00:00', FALSE, TRUE);
+    -- Set the initial values for the has_time column. For old trips, if they are set
+    -- to midnight, they are assumed to have no time portion.
+    UPDATE `trips` SET `has_time` = IF(DATE_FORMAT(`timestamp`, '%H:%i:%S') = '00:00:00', FALSE, TRUE);
 COMMIT;
