bc2e9fa42a747eee1549054d53c13f0c82ecd5fc
dasdware <info@dasdware.de>
2023-10-09 21:01:11 +0200

    #3 Add possibility of different accumulation types in frontend

--- client/lib/queries.dart
+++ client/lib/queries.dart
@@ -1,5 +1,6 @@
 import 'package:dw_bike_trips_client/session/operations/timestamp.dart';
 import 'package:dw_bike_trips_client/session/trip.dart';
+import 'package:jiffy/jiffy.dart';
 
 const serverInfo = """
   query serverInfo {
@@ -85,15 +86,148 @@ const trips = """
   }
 """;
 
-String dashboard(Timestamp from, Timestamp to) {
+// Modes:
+//  14 Days
+//  12 Weeks
+//  12 Months (1 year)
+//  10 Years
+
+enum AccumulationKind { days, weeks, months, years, all }
+
+class AccumulationQueryParameters {
+  final AccumulationKind accumulationKind;
+  final Timestamp? from;
+  final Timestamp to;
+
+  AccumulationQueryParameters(this.accumulationKind)
+      : to = Timestamp.now(),
+        from = AccumulationQueryParameters._calculateFrom(accumulationKind);
+
+  static int _calculateItemCount(
+    AccumulationKind accumulationKind,
+  ) {
+    switch (accumulationKind) {
+      case AccumulationKind.days:
+        return 14;
+      case AccumulationKind.weeks:
+        return 12;
+      case AccumulationKind.months:
+        return 12;
+      case AccumulationKind.years:
+        return 5;
+      case AccumulationKind.all:
+        return 10;
+    }
+  }
+
+  static Timestamp? _calculateFrom(
+    AccumulationKind accumulationKind,
+  ) {
+    var to = Timestamp.now();
+    switch (accumulationKind) {
+      case AccumulationKind.days:
+        return to.subtract(
+            days: AccumulationQueryParameters._calculateItemCount(
+                accumulationKind));
+      case AccumulationKind.weeks:
+        return to.subtract(
+            weeks: AccumulationQueryParameters._calculateItemCount(
+                accumulationKind));
+      case AccumulationKind.months:
+        return to.subtract(
+            months: AccumulationQueryParameters._calculateItemCount(
+                accumulationKind));
+      case AccumulationKind.years:
+        return to.subtract(
+            years: AccumulationQueryParameters._calculateItemCount(
+                accumulationKind));
+      case AccumulationKind.all:
+        return to.subtract(
+            years: AccumulationQueryParameters._calculateItemCount(
+                accumulationKind));
+    }
+  }
+
+  String get range {
+    if (from == null) {
+      return """{to: "${to.toString()}"}""";
+    } else {
+      return """{from: "${from.toString()}", to: "${to.toString()}"}""";
+    }
+  }
+
+  String get grouping {
+    switch (accumulationKind) {
+      case AccumulationKind.days:
+        return "day";
+      case AccumulationKind.weeks:
+        return "week";
+      case AccumulationKind.months:
+        return "month";
+      case AccumulationKind.years:
+      case AccumulationKind.all:
+        return "year";
+    }
+  }
+
+  int get itemCount {
+    return AccumulationQueryParameters._calculateItemCount(accumulationKind);
+  }
+
+  int get toItemNumber {
+    switch (accumulationKind) {
+      case AccumulationKind.days:
+        return to.day;
+      case AccumulationKind.weeks:
+        return to.week;
+      case AccumulationKind.months:
+        return to.month;
+      case AccumulationKind.years:
+      case AccumulationKind.all:
+        return to.year % 100;
+    }
+  }
+
+  int get prevMaxItemNumber {
+    switch (accumulationKind) {
+      case AccumulationKind.days:
+        return Jiffy.parseFromDateTime(to.toDateTime())
+            .subtract(months: 1)
+            .daysInMonth;
+      case AccumulationKind.weeks:
+        return 52;
+      case AccumulationKind.months:
+        return 12;
+      case AccumulationKind.years:
+      case AccumulationKind.all:
+        return 99;
+    }
+  }
+
+  int get toGroupNumber {
+    switch (accumulationKind) {
+      case AccumulationKind.days:
+        return to.year * 100 + to.month;
+      case AccumulationKind.weeks:
+        return to.year;
+      case AccumulationKind.months:
+        return to.year;
+      case AccumulationKind.years:
+      case AccumulationKind.all:
+        return to.year ~/ 100;
+    }
+  }
+}
+
+String dashboard(AccumulationQueryParameters parameters) {
   return """
     query completeDashboard {
       dashboard {
         distances {today yesterday thisWeek lastWeek thisMonth lastMonth thisYear lastYear allTime }
       }
       accumulateTrips(
-        range: {from: "${from.toString()}", to: "${to.toString()}"},
-        grouping: month
+        range: ${parameters.range},
+        grouping: ${parameters.grouping}
       ) {
         name
         begin
